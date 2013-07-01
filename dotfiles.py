03c11f2842ffc3fbf10534ac799cb8b9928f3698
Samuel Kohonen <deverant@gmail.com>
2013-07-01 08:43:20 +0200

    pep8ify

--- dotfiles.py
+++ dotfiles.py
@@ -12,11 +12,12 @@ import shutil
 
 # A list of regular expressions used to skip files while
 # generating the file manifest for copying
-IGNORE_REGEX=[r'README\.md', r'dotfiles\.py', r'^\.', r'~$']
+IGNORE_REGEX = [r'README\.md', r'dotfiles\.py', r'^\.', r'~$']
 
 home_dir = os.path.expanduser("~")
 
 IGNORE_REGEX = re.compile("|".join(IGNORE_REGEX))
+
 
 def get_file_manifest(path):
     files = []
@@ -25,9 +26,10 @@ def get_file_manifest(path):
             dirnames.remove(d)
         for f in filenames:
             if not IGNORE_REGEX.search(f):
-                files.append((root, root.replace(path,""), f))
+                files.append((root, root.replace(path, ""), f))
 
     return files
+
 
 def ensure_dir(path):
     d = os.path.dirname(path)
@@ -41,16 +43,16 @@ def ensure_dir(path):
 def main(argv):
     files = get_file_manifest(os.getcwd())
 
-    for root,rel,f in files:
+    for (root, rel, f) in files:
         if rel:
-            target = "."+os.path.join(rel[1:], f)
+            target = "." + os.path.join(rel[1:], f)
         else:
-            target = "."+f
+            target = "." + f
         src = os.path.join(root, f)
         target = os.path.join(home_dir, target)
-        print "copy",src,"-->",target
+        print "copy", src, "-->", target
         ensure_dir(target)
-        shutil.copy2(os.path.join(root,f), target)
+        shutil.copy2(os.path.join(root, f), target)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main(sys.argv)
