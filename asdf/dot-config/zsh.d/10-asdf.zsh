3081e90790fe716e9b9b31ab9bb21dae0b4bcce1
Samuel Kohonen <samuel@deverant.com>
2023-07-10 11:08:51 -0400

    Support GOROOT through asdf and remove nag

--- asdf/dot-config/zsh.d/10-asdf.zsh
+++ asdf/dot-config/zsh.d/10-asdf.zsh
@@ -2,9 +2,17 @@ if [ -r "${HOME}/.asdf/asdf.sh" ]
 then
     source "${HOME}/.asdf/asdf.sh"
 
+    # only look at .go-version
+    export ASDF_GOLANG_MOD_VERSION_ENABLED=false
+
     if [ -r "${HOME}/.asdf/plugins/java/set-java-home.zsh" ]
     then
-        source "${HOME}/.asdf/plugins/java/set-java-home.zsh"
+	source "${HOME}/.asdf/plugins/java/set-java-home.zsh"
+    fi
+
+    if [ -r "${HOME}/.asdf/plugins/golang/set-env.zsh" ]
+    then
+	source "${HOME}/.asdf/plugins/golang/set-env.zsh"
     fi
 
     # initialise completions with ZSH's compinit
